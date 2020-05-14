bd76cf83b88d7241883adc3ec0ff44cef78c874a
neyriks <mesuppgg@gmail.com>
2020-05-15 00:20:20 +0300

    Поправил меню

--- src/modules/dropMenu.js
+++ src/modules/dropMenu.js
@@ -4,7 +4,7 @@ const dropMenu = () => {
     let btnMenu = document.querySelector('.club-select'),
     menu = document.querySelector('.club-select .clubs-list ul');
     btnMenu.addEventListener('click', () => {
-        let menuUl = document.querySelector('.club-select .clubs-list ul').style.display;
+        let menuUl = document.querySelector('.club-select .clubs-list ul').style.display = 'none';
         if(menuUl === 'none') {
             menu.style.display = 'block';
         } else {
