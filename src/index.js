599bcf8ea40391bd52e316d85b3554ca8ebc8823
neyriks <mesuppgg@gmail.com>
2020-05-14 23:47:40 +0300

    Подарок

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import dropMenu from './modules/dropMenu';
 dropMenu();
 import togglePopUp from './modules/togglePopUp';
 togglePopUp();
+import gift from './modules/gift';
+gift();
---
+++ src/modules/gift.js
@@ -0,0 +1,51 @@
+'use strict';
+const gift = () => {
+    const giftBtn = document.querySelector('.fixed-gift'),
+        gift = document.querySelector('#gift'),
+        closeGift = document.querySelector('#gift .close-form'),
+        overlay = document.querySelector('#gift .overlay');
+    giftBtn.addEventListener('click', () => {
+        gift.style.display = 'block';
+        gift.style.opacity = 0;
+
+        function animate({
+            duration,
+            draw,
+            timing
+        }) {
+            const start = performance.now();
+            requestAnimationFrame(function animate(time) {
+                let timeFraction = (time - start) / duration;
+                if (timeFraction > 1) {
+                    timeFraction = 1;
+                }
+                const progress = timing(timeFraction);
+                draw(progress);
+                if (timeFraction < 1) {
+                    requestAnimationFrame(animate);
+                }
+            });
+        }
+        animate({
+            duration: 500,
+            timing(timeFraction) {
+                return timeFraction;
+            },
+            draw(progress) {
+                gift.style.opacity = progress * 1;
+            },
+        });
+        giftBtn.style.display = 'none';
+    });
+    // Закрываем Popup
+    closeGift.addEventListener('click', () => {
+        gift.style.display = 'none';
+    });
+    // Закрываем через подложку.
+    overlay.addEventListener('click', (e) => {
+        if(e.target === overlay) {
+            gift.style.display = 'none';
+        }
+    });
+};
+export default gift;
