c9b421c4427e0f5ad9ed42934f0ef51a129c387a
neyriks <mesuppgg@gmail.com>
2020-05-15 17:51:25 +0300

    Бургер меню открывается

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import togglePopUp from './modules/togglePopUp';
 import gift from './modules/gift';
 import callback from './modules/callback';
 import topMenu from './modules/topmenu';
+import openBurger from './modules/openBurger';
 
 dropMenu();
 togglePopUp();
 gift();
 callback();
 topMenu();
+openBurger();
